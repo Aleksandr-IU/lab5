8603dec2a22bd34c0d2630893148f9fb2105f2f9
Aleksandr-IU <32134070+Aleksandr-IU@users.noreply.github.com>
2017-12-27 13:56:22 +0300

    Add files via upload

---
+++ 5 лаба/lab5/Forma.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Forma : Form
+    {
+        List<string> words = new List<string>();
+
+        public Forma()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+
+            Stopwatch time = new Stopwatch();
+            time.Start();
+
+            string text = File.ReadAllText(dialog.FileName);
+            string[] textArray = Regex.Split(text, "\\W");
+            foreach (string word in textArray)
+            {
+                string trimmedWord = word.Trim();
+                if (!words.Contains(trimmedWord))
+                    words.Add(trimmedWord);
+            }
+            textBoxText.Text = text.ToString();
+            time.Stop();
+            label.Text = "Time for file load: " + time.Elapsed.ToString();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string wordToFind = textBoxWord.Text.Trim();
+            textBoxWord.Text = wordToFind;
+            int maxDistance = Convert.ToInt32(textBoxDist.Text);
+
+            if (string.IsNullOrEmpty(wordToFind) && words.Count <= 0) return;
+
+            string wordToFindUpper = wordToFind.ToUpper();
+            List<string> tempList = new List<string>();
+
+            Stopwatch time = new Stopwatch();
+            time.Start();
+
+            foreach (string str in words)
+                if (str.ToUpper().Contains(wordToFindUpper)
+                    && Distance.CalculateDistance(str.ToUpper(), wordToFindUpper)
+                    <= maxDistance)
+                    tempList.Add(str);
+
+            time.Stop();
+            label.Text = "Time for word search " + time.Elapsed.ToString();
+            listBox.BeginUpdate();
+            listBox.Items.Clear();
+
+            foreach (string str in tempList)
+            {
+                listBox.Items.Add(str);
+            }
+            listBox.EndUpdate();
+
+        }
+    }
+}
